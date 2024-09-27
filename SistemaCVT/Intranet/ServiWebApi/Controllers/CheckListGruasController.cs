0b79cdfda4366b64d76855cf3593a93bb841fcbe
Usuario Desarrollo <desarrollo@CVT.local>
2024-09-27 09:46:29 -0300

    Se agregan cambios a DBMLDESAINT tabla Grua y se crea nuevo controlador CheckListGrua

---
+++ SistemaCVT/Intranet/ServiWebApi/Controllers/CheckListGruasController.cs
@@ -0,0 +1,54 @@
+﻿using DBMermasRecepcion;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common.CommandTrees;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiWebApi.Controllers
+{
+    [RoutePrefix("api/CheckListGruas")]
+    public class CheckListGruasController : ApiController
+    {
+        DBMLCVTDESAINTDataContext dbDsa = new DBMLCVTDESAINTDataContext();
+
+        [HttpGet]
+        [Route("TiposMaquinaria")]
+        public List<string> ObtieneTiposMaquinaria()
+        {
+            List<string> ret = new List<string>();
+            try
+            {
+                ret = (
+                    from l in dbDsa.CVT_Gruas
+                    select l.Tipo).Distinct().ToList<string>();
+            }
+            catch (Exception)
+            {
+
+            }
+            return ret;
+        }
+
+        [HttpGet]
+        [Route("NumeroGrua")]
+        public List<string> ObtieneNumeroGrua()
+        {
+            List<string> ret = new List<string>();
+            try
+            {
+                ret = (
+                    from l in dbDsa.CVT_Gruas
+                    select l.Grua_Numero).Distinct().ToList<string>();
+            }
+            catch (Exception)
+            {
+
+            }
+            return ret;
+        }
+    }
+}
